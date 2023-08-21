086d287d8fc788ee56b9f1927b7d0d0e797c1da5
Hlobile Shongwe <62103682+Hlobile@users.noreply.github.com>
2023-08-21 19:18:57 +0200

    Add project files.

---
+++ TranslateGPT/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TranslateGPT.Models;
+
+namespace TranslateGPT.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        private readonly List<string> mostUsedLanguages = new List<string>()
+        {
+            "English",
+            "Mandarin Chinese",
+            "Hindi",
+            "Spanish",
+            "French",
+            "Arabic",
+            "Bengali",
+            "Russian",
+            "Portuguese",
+            "Urdu",
+            "Indonesian",
+            "German",
+            "Japanese",
+            "Swahili",
+            "Zulu",
+            "Punjabi"
+
+        };
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
